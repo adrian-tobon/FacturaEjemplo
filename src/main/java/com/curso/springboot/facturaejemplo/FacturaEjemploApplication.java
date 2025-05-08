635e2f3126b2ea9ee10a018348a16f9c840ae39c
ctobonpe <ctobonpe@gmail.com>
2025-05-08 09:58:18 -0500

    first commit

---
+++ src/main/java/com/curso/springboot/facturaejemplo/FacturaEjemploApplication.java
@@ -0,0 +1,13 @@
+package com.curso.springboot.facturaejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturaEjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturaEjemploApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/springboot/facturaejemplo/FacturaEjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.springboot.facturaejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturaEjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

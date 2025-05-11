465e108b1c002db42c03c21a9e6ad88f0cd07b29
ctobonpe <ctobonpe@gmail.com>
2025-05-11 09:24:22 -0500

    correcciones finales

--- src/main/java/com/curso/springboot/facturaejemplo/controllers/InvoiceController.java
+++ src/main/java/com/curso/springboot/facturaejemplo/controllers/InvoiceController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.curso.springboot.facturaejemplo.models.Client;
+//import com.curso.springboot.facturaejemplo.models.Client;
 import com.curso.springboot.facturaejemplo.models.Invoice;
 
 @RestController
